47fc1ce72dc7fd2ab6e66d34329fd57b0881a134
edvs19 <edvs19@inf.ufpr.br>
2020-05-31 20:01:35 -0300

    Error test

--- js/worldwind/worldwind.js
+++ js/worldwind/worldwind.js
@@ -100,7 +100,7 @@ function posicionarpin(){
 }
 
 function plotar(){
-    if(pinatual != null && highlightedItems == []){
+    if(pinatual != null && highlightedItems != []){
         alert(pinatual);
     }
 }
